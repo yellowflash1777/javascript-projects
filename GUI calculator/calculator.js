dc4e72514849537718265cd83c5fc0997a44a489
Prashant Prithyani <prithyani2002@gmail.com>
2022-10-15 11:51:18 +0530

    created gui calculator

---
+++ GUI calculator/calculator.js
@@ -0,0 +1,47 @@
+function calculator() {
+  expression=document.getElementById("expression").value;
+  
+
+ if(expression){
+
+    try{
+        console.log('Expression',expression);
+        answer=eval(expression);
+        document.getElementById("expression").value=answer;
+        console.log('Answer',answer);
+        addlog(expression+'='+answer);
+    }
+    catch(err){
+        console.log('error',err);
+        addlog(expression+' is bad expression. Please fix it.')
+    }
+
+}}
+
+function addlog(msg){
+    var ul =document.getElementById("log")
+    var li =document.createElement("li");
+    var msg_text_node =document.createTextNode(msg);
+    li.appendChild(msg_text_node);
+    ul.appendChild(li);
+    
+}
+function math_calculator() {
+    expression=document.getElementById("expression").value;
+    
+  
+   if(expression){
+  
+      try{
+          console.log('Expression',expression);
+          answer=math.evaluate(expression);
+          document.getElementById("expression").value=answer;
+          console.log('Answer',answer);
+          addlog(expression+'='+answer);
+      }
+      catch(err){
+          console.log('error',err);
+          addlog(expression+'is bad. Please fix it.')
+      }
+  
+  }}
